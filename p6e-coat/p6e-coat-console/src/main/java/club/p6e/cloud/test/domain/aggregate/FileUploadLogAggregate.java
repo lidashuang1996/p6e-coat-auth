744ddf2c8c141e5ebee62e63378b1aa35ff9dba2
lidashuang <1294935733@qq.com>
2023-07-06 08:18:20 +0800

    添加文件功能目录的信息的获取

--- p6e-coat/p6e-coat-console/src/main/java/club/p6e/cloud/test/domain/aggregate/FileUploadLogAggregate.java
+++ p6e-coat/p6e-coat-console/src/main/java/club/p6e/cloud/test/domain/aggregate/FileUploadLogAggregate.java
@@ -2,17 +2,15 @@ package club.p6e.cloud.test.domain.aggregate;
 
 import club.p6e.cloud.test.Properties;
 import club.p6e.cloud.test.domain.ConfigurationDomain;
-import club.p6e.cloud.test.infrastructure.model.FileUploadModel;
-import club.p6e.cloud.test.infrastructure.repository.FileUploadRepository;
+import club.p6e.cloud.test.domain.keyvalue.FileUploadLogKeyValue;
 import com.darvi.hksi.badminton.lib.utils.SpringUtil;
-import jakarta.persistence.criteria.Predicate;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.jpa.domain.Specification;
+import lombok.Data;
+import lombok.experimental.Accessors;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.File;
+import java.io.Serializable;
+import java.time.Duration;
+import java.time.LocalDateTime;
 
 /**
  * @author lidashuang
@@ -20,10 +18,80 @@ import java.util.List;
  */
 public class FileUploadLogAggregate extends ConfigurationDomain {
 
-    public static void storage() {
+    private static LocalDateTime LOCAL_DATE_TIME = null;
+    private static FileUploadLogKeyValue.Storage STORAGE = null;
+
+    private static final long CACHE_MAX_INTERVAL = 3600;
+
+    public static FileUploadLogKeyValue.Storage storage() {
+        if (LOCAL_DATE_TIME == null || STORAGE == null) {
+            init();
+        } else {
+            final LocalDateTime now = LocalDateTime.now();
+            final Duration duration = Duration.between(now, LOCAL_DATE_TIME);
+            if (duration.toSeconds() > CACHE_MAX_INTERVAL) {
+                init();
+            }
+        }
+        return STORAGE;
+    }
+
+    private static synchronized void init() {
+        if (LOCAL_DATE_TIME == null || STORAGE == null) {
+            load();
+        } else {
+            final LocalDateTime now = LocalDateTime.now();
+            final Duration duration = Duration.between(now, LOCAL_DATE_TIME);
+            if (duration.toSeconds() > CACHE_MAX_INTERVAL) {
+                load();
+            }
+        }
+    }
+
+    private static void load() {
         final Properties properties = SpringUtil.getBean(Properties.class);
         final String path = properties.getFileUpload().getPath();
-        System.out.println(">>>>>>>> " + path);
+        final Model rootModel = new Model().setPath(path);
+        compute(rootModel);
+        LOCAL_DATE_TIME = LocalDateTime.now();
+        STORAGE = new FileUploadLogKeyValue.Storage()
+                .setFiles(rootModel.getFile())
+                .setMemory(rootModel.getLength())
+                .setFolders(rootModel.getFolder());
+    }
+
+    private static void compute(Model model) {
+        final File f = new File(model.getPath());
+        if (f.exists()) {
+            final File[] fs = f.listFiles();
+            if (fs != null && fs.length > 0) {
+                for (final File item : fs) {
+                    if (item.isFile()) {
+                        model.setFile(model.getFile() + 1);
+                        model.setLength(item.length());
+                    } else if (item.isDirectory()) {
+                        final Model dirModel = new Model().setPath(item.getAbsolutePath());
+                        compute(dirModel);
+                        model.setFile(model.getFile() + dirModel.getFile());
+                        model.setLength(model.getLength() + dirModel.getLength());
+                        model.setFolder(model.getFolder() + dirModel.getFolder() + 1);
+                    }
+                }
+            } else {
+                model.setLength(0L);
+            }
+        } else {
+            model.setLength(-1L);
+        }
+    }
+
+    @Data
+    @Accessors(chain = true)
+    private static class Model implements Serializable {
+        private String path;
+        private int file;
+        private int folder;
+        private long length;
     }
 
 }
